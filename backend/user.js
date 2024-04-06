8767aa8c325483499eb5963a27ed8fd058669db6
iqabbal <ilyas.qabbal.2020@usms.ac.ma>
2024-04-06 05:38:05 -0400

    fixed signU and signIn

--- backend/user.js
+++ backend/user.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const TodoModel = require('./models/Todo');
 
+//password hashing
 
+const bcrypt = require('bcrypt')
 router.post("/signup", (req, res) => {
     
 console.log("is here");
@@ -12,38 +14,34 @@ console.log("is here");
     password = password.trim();
     dateOfBirth = dateOfBirth.trim();
 
-    console.log(name);
-    console.log(email);
-    console.log(password);
-    console.log(dateOfBirth);
     if (name === "" || email === "" || password === "" || dateOfBirth === "") {
 	    console.log("empty");
         res.json({
-            status: "failed",
+            status: "FAILED",
             message: "Empty input fields",
         });
     } else if (!/^[a-zA-Z]*$/.test(name)) {
 	    console.log("invalid name");
         res.json({
-            status: "failed",
+            status: "FAILED",
             message: "Invalid name entered",
         });
     } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
 	    console.log("invalid email");
         res.json({
-            status: "failed",
+            status: "FAILED",
             message: "Invalid email entered",
         });
     } else if (!Date.parse(dateOfBirth)) {
 	    console.log("invalid date");
         res.json({
-            status: "failed",
+            status: "FAILED",
             message: "Invalid date entered",
         });
     } else if (password.length < 8) {
 	    console.log("invalid password");
         res.json({
-            status: "failed",
+            status: "FAILED",
             message: "Password is too short",
         });
     } else {
@@ -52,28 +50,45 @@ console.log("is here");
                 if (result.length) {
 		    console.log("email deja kayn");
                     res.json({
-                        status: "failed",
+                        status: "FAILED",
                         message: "User with the provided email already exists",
                     });
                 } else {
 		    console.log("valid info");
 
                     // You can create a new user here
-                    TodoModel.create({name : name, email: email , password:password , dateOfBirth : dateOfBirth}).then(()=>{
+            const saltRounds =10;
+            bcrypt.hash(password,saltRounds).then((hashedPassword)=>{
 
-console.log("Valid info");                            				res.json({                                                status: "success",                                    message: "User signed up successfully",                                                                 });
-
-
-		    }).catch(()=>{
-			    res.json({                                                status: "failed",                                    message: "an error occured when adding data",                                                                 })
-		    });
-
+                TodoModel.create({name : name, email: email , password:hashedPassword , dateOfBirth : dateOfBirth})
+                .then(()=>{                          				
+                    res.json({                    
+                        status: "SUCCESS",
+                        message: "User signed up successfully", 
+                    });
+    
+                }).catch(()=>{
+                    res.json({              
+                        status: "FAILED",
+                        message: "an error occured when adding data",                                                                 
+                    })
+                });
+                
+            }).catch(()=>{
+                    res.json({              
+                        status: "FAILED",
+                        message: "an error occured when hashing password",                                                                 
+                    })
                 }
+            )
+
+
+            }
             })
             .catch((err) => {
                 console.log(err);
                 res.json({
-                    status: "failed",
+                    status: "FAILED",
                     message: "An error occurred while checking for existing user",
                 });
             });
@@ -81,39 +96,59 @@ console.log("Valid info");                            				res.json({            
 });
 
 router.post("/signin", (req, res) => {
-    // Logic for signin route
- 	console.log("signin");                            let { email, password } = req.body;
+                       
+    let { email, password } = req.body;
+
 	email = email.trim();
 	password = password.trim();
-	console.log("password : ", password);
-	if ( email === "" || password === ""
- ) {
+	if ( email == "" || password == "") {
         console.log("empty");
         res.json({
-            status: "failed",                                     message: "Empty input fields",
+            status: "FAILED",                                     
+            message: "Empty input fields",
         });
 	}else{
 		//check the pass
-		TodoModel.find({email}).then((data)=>{
-
-			console.log("data" , data);
-			console.log("data 0 " , data[0]);
-			if(data[0].password === password ){
-				console.log("mar7ba bik");
-			}
-			else{
-				console.log("pass incorrect");
-			}
-		}).catch((err)=>{
-
-
-		console.log(err);                                     res.json({
-                    status: "failed",
-                    message: "An error occurred while getting the user",                                          });
-
-
-
-		});
+		TodoModel.find({ email }).then((data) => {
+            console.log(data);
+            if (data.length) {
+                const hashedPassword = data[0].password;
+                console.log("hashed pass: ", hashedPassword);
+                bcrypt.compare(password, hashedPassword)
+                    .then((data) => {
+                        if (data) {
+                            res.json({
+                                status: "SUCCESS",
+                                message: "signin successful",
+                                data: data
+                            });
+                        } else {
+                            res.json({
+                                status: "FAILED",
+                                message: "invalid password entered !!",
+                            });
+                        }
+                    }).catch((err) => {           
+                        console.error("An error occurred while comparing password:", err);
+                        res.json({
+                            status: "FAILED",
+                            message: "An error occurred while comparing password",
+                        });
+                    });
+            } else {       
+                res.json({
+                    status: "FAILED",
+                    message: "invalid conditional entered",
+                });
+            }
+        }).catch((err) => {
+            console.error("An error occurred while checking for existing user:", err);
+            res.json({
+                status: "FAILED",
+                message: "An error occurred while checking for existing user",
+            });
+        });
+        
 	}
 
 
